c98dc467c6ce98127a72d2033758d15aab96be17
User <User@Desktop-Anndy.lan>
2025-02-14 10:43:26 -0600

    Semana5: finalizado categoria

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -17,7 +17,29 @@ public class CategoriaService {
     @Transactional(readOnly=true)
     public List<Categoria> getCategorias(boolean activos){
         var lista = categoriaRepository.findAll();
-        
+        //Falta algo aca
         return lista;
     }
+    
+    //Se escribe los metodos de un CRUD Create Read Update Delete
+    
+    @Transactional(readOnly=true)
+    public Categoria getCategoria(Categoria categoria){
+        
+        return categoriaRepository.findById(categoria.getIdCategoria()).orElse(null);
+    }
+    
+    @Transactional
+    public void delete(Categoria categoria){
+        //Elimina el registro que contiene el id igual a lo pasado en categoria.getIdCategoria()
+         categoriaRepository.delete(categoria);
+    }
+    
+    @Transactional
+    public void save(Categoria categoria){
+        //si categoria.idCategoria esta vacio.. se inserta un registro
+        //si categoria.idCategoria tiene algo.. se modifica ese registro
+         categoriaRepository.save(categoria);
+    }
+    
 }
